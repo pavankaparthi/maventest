28c793703c874b71020fc8a0a5451a6a7f392d4e
pavan.k <kaparthi.pavan@gmail.com>
2020-05-20 11:11:10 +0530

    updated class 3

--- Class3.java
+++ Class3.java
@@ -2,7 +2,8 @@
 public class Class3 {
 
 	public static void main(String[] args) {
-		System.out.println("This is class 3");
+		System.out.println("This is class 3); 
+		System.out.println("Updated class 3 with a new line of code");
 
 	}
 
